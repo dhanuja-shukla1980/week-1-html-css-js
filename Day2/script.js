f0a12237b1595030dbfdc651f9b07c6821551d2d
dhanuja-shukla1980 <dhanujashukla2007@gmail.com>
2025-06-09 13:27:39 +0530

    Delete Day2 directory

--- Day2/script.js
+++
@@ -1,6 +0,0 @@
-let button1 = document.getElementsByClassName("btn");
-let button2 = document.getElementById("btn2");
-
-// console.log(button1);
-console.log(button2);
-button2.innerHTML = " Hi JavaScript ";
