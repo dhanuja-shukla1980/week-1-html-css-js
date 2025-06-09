13d47fffed12aff8b9f49ff3a1ffd3fd472f4f5c
dhanuja-shukla1980 <dhanujashukla2007@gmail.com>
2025-06-09 13:18:31 +0530

    file upload

---
+++ Day2/script.js
@@ -0,0 +1,6 @@
+let button1 = document.getElementsByClassName("btn");
+let button2 = document.getElementById("btn2");
+
+// console.log(button1);
+console.log(button2);
+button2.innerHTML = " Hi JavaScript ";
